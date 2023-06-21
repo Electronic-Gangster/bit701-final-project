bbebc7605e1d092396186c4dd481d2566202b019
HYUNG MIN KIM <freshtuna88@hotmail.com>
2023-06-21 16:07:27 +0900

    첫 커밋

---
+++ src/main/reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import img from './2.gif';
+
+function App() {
+  return (
+    <div className="App">
+      <img alt='' src={img} width={300} border="1"/>
+      <br></br><br></br>
+      <h2>Docker 배포 성공할 때 까지 숨 참는다.</h2>
+    </div>
+  );
+}
+
+export default App;
