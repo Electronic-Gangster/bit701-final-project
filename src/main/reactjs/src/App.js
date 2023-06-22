b67e85ec9401a63b4aa5d28063f6e2690805d771
HYUNG MIN KIM <freshtuna88@hotmail.com>
2023-06-22 16:14:09 +0900

    배포 성공

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import img from './2.gif';
+import img1 from './1.gif';
+import img2 from './3.gif';
+import img3 from './4.gif';
 
 function App() {
   return (
     <div className="App">
       <img alt='' src={img} width={300} border="1" className=''/>
+      <img alt='' src={img1} width={300} border="1" className=''/>
+      <img alt='' src={img2} width={300} border="1" className=''/>
+      <img alt='' src={img3} width={300} border="1" className=''/>
       <br></br><br></br>
-      <h1 className='bb2'>Docker 배포 성공할 때 까지 숨 참는다.</h1>
+      <h1 className='bb2'>Docker 배포 성공했기 때문에 이제 숨 쉰다!</h1>
     </div>
   );
 }
