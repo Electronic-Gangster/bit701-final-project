589377ece97a5d1480b240b4f130055903d171e2
HYUNG MIN KIM <freshtuna88@hotmail.com>
2023-06-22 16:17:03 +0900

    도커수정2

--- src/main/reactjs/src/App.js
+++ src/main/reactjs/src/App.js
@@ -7,10 +7,10 @@ import img3 from './4.gif';
 function App() {
   return (
     <div className="App">
-      <img alt='' src={img} width={300} border="1" className=''/>
-      <img alt='' src={img1} width={300} border="1" className=''/>
-      <img alt='' src={img2} width={300} border="1" className=''/>
-      <img alt='' src={img3} width={300} border="1" className=''/>
+      <img alt='' src={img} width={300} height={300} border="1" className=''/>
+      <img alt='' src={img1} width={300} height={300} border="1" className=''/>
+      <img alt='' src={img2} width={300} height={300} border="1" className=''/>
+      <img alt='' src={img3} width={300} height={300} border="1" className=''/>
       <br></br><br></br>
       <h1 className='bb2'>Docker 배포 성공했기 때문에 이제 숨 쉰다!</h1>
     </div>
