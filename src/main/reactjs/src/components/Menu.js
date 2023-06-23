9db6614ad3a58cc1308235da98c2864ac5fc5c19
HYUNG MIN KIM <freshtuna88@hotmail.com>
2023-06-23 10:05:52 +0900

    0623 오전

---
+++ src/main/reactjs/src/components/Menu.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../App.css';
+
+function Menu(props) {
+    return (
+        <div>
+            <h1>메뉴</h1>
+        </div>
+    );
+}
+
+export default Menu;
---
+++ src/main/reactjs/src/member/index.js
@@ -0,0 +1,3 @@
+export {default as MemeberForm} from './MemeberForm';
+export {default as Memberlist} from './Memberlist';
+export {default as LoginForm} from './LoginForm';
